eabcc84b526c4ecc9ca73f0a4adb28423a53479a
JanviBhalala <59002192+JanviBhalala@users.noreply.github.com>
2020-01-02 09:47:36 +0530

    ProjectSolutions

---
+++ ProjectSolutions/Practical1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Practical1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern1();
+            Console.WriteLine();
+            Pattern2();
+            Console.WriteLine();
+            Pattern3();
+            Console.WriteLine();
+            Pattern4();
+            
+            Console.ReadKey();
+        }
+
+        static void Pattern1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+        static void Pattern2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+        }
+        static void Pattern3()
+        {
+            for (int i = 0; i < 5; i++)
+            { 
+
+                for (int k = 1; k <= i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Pattern4()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 4; j>=0; j--)
+                {
+                    Console.Write("");
+                }
+
+                for (int k = 1; k <= i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
